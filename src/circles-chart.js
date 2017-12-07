b129089ae13082ddf5c835f580b5a5a8fb8e6a1a
Spyros Nikolakis <nikolakispyros@gmail.com>
2017-12-07 17:02:51 +0200

    chart configuration changes, do not display competence areas

--- src/circles-chart.js
+++ src/circles-chart.js
@@ -1,10 +1,20 @@
-/*eslint no-unused-vars: "off"*/
 import {competences} from './data'
 import '../lib/circles-chart/carrotsearch.circles.js'
 
+// configuration constant variables
 const alphas = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+const colorsForAreas = ['#0000ff', '#ff0000', '#00ff00']
+const groupSelectionOutlineColor = '#30415d' // $blue
+const groupSelectionColor = getRGBAString('#000000', 0.15)
 
+/**
+ * Transform hex color string to RGBA color string
+ * @param hex The color as hex
+ * @param alpha (optional) The alpha, used in the RGBA string (default: 1)
+ * @returns {string}
+ */
 function getRGBAString(hex, alpha) {
+  alpha = alpha || 1
   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   let red = parseInt(result[1], 16)
   let green =  parseInt(result[2], 16)
@@ -12,16 +22,15 @@ function getRGBAString(hex, alpha) {
   return 'rgba(' + red + ', ' + green + ', ' + blue + ', ' + alpha + ')'
 }
 
-function customAttributes(opts, props, vars) {
+function customColorDecorator(opts, props, vars) {
   vars.groupColor = getRGBAString(props.group.gcolor, (props.level === 0) ? 0.7 : alphas[props.index])
   vars.labelColor = '#000'
 }
 
 function formatCompetenciesToGroups() {
-  const colorsForAreas = ['0000ff', 'ff0000', '00ff00']
   let competenceAreasGroups = []
+  let competencesGroups = []
   for (let i = 0; i < competences.length; i++) { // competence areas
-    let competencesGroups = []
     for (let j = 0; j < competences[i].competences.length; j++) { // competences
       let skillsGroups = []
       for (let k = 0; k < competences[i].competences[j].skills.length; k++) { // skills
@@ -31,21 +40,35 @@ function formatCompetenciesToGroups() {
     }
     competenceAreasGroups.push({label: competences[i].name, gcolor: colorsForAreas[i], groups: competencesGroups})
   }
-  return competenceAreasGroups
+  return competencesGroups
 }
 
-const circles = new window.CarrotSearchCircles({
+// initialize chart
+let circles = new window.CarrotSearchCircles({
   id: 'chart',
-  groupColorDecorator: customAttributes,
+  groupColorDecorator: customColorDecorator,
   dataObject: {
     groups: formatCompetenciesToGroups()
   },
   ringScaling: 1,
-  pullbackTime: 2,
-  pullbackAnimation: 'tumbler',
-  groupSelectionOutlineColor: 'rgba(48, 65, 93, 1)', // $blue
-  groupSelectionColor: 'rgba(3, 20, 36, 0.15)', // $dark-blue
+  groupSelectionOutlineColor: groupSelectionOutlineColor,
+  groupSelectionColor: groupSelectionColor,
   noTexturingCurvature: 0,
   radialTextureStep: 50,
   angularTextureStep: 15,
+  groupFontFamily: 'sans-serif',
+  groupMinFontSize: '20%',
+  groupMaxFontSize: '20%',
+  rolloutAnimation: 'tumble',
+  visibleGroupCount: 0, // display all groups
+  titleBar: 'inscribed',
+  titleBarTextColor: "#000",
+  titleBarMinFontSize: 18,
+  titleBarMaxFontSize: 18,
+})
+
+// event handlers
+window.addEventListener("resize", function() {
+  // on window resize, resize circles chart as well
+  circles.resize()
 })
