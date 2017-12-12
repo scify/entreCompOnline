a401529374e198acc45dd7f92da68cd9fb852768
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2017-12-12 18:36:13 +0200

    improve footer html

--- src/components/CompetencesFooter.vue
+++ src/components/CompetencesFooter.vue
@@ -1,31 +1,27 @@
 <template>
-  <footer>
-    <div class="footer-grid">
-      <div class="footer-L section-material-design color-indigo">&nbsp;</div>
-      <div v-if="previousCompetence" class="footer-grid-L section-material-design color-indigo">
-        <a class="footer-text" href="javascript:void(0)"
-           @click="selectCompetence(previousCompetence)">
-          <i class="material-icons arrow-L" aria-hidden="true">arrow_back</i>
-          <span class="direction">Previous</span><span class="screen-reader">:</span>
-          <div>
-            {{previousCompetence.name}}
-
-          </div>
-        </a>
-      </div>
-      <div class="footer-R section-what-is-material color-indigo">&nbsp;</div>
-      <div class="footer-grid-R section-what-is-material color-indigo">
-        <a v-if="nextCompetence" class="footer-text" href="javascript:void(0)"
-           @click="selectCompetence(nextCompetence)">
-          <i class="material-icons arrow-R" aria-hidden="true">arrow_forward</i>
-          <span class="direction">Next</span><span class="screen-reader">:</span>
-          <div>
-            {{nextCompetence.name}}
-
-          </div>
-        </a>
+  <footer :class="'comp-area-'+competenceArea.id">
+    <div class="demo-grid mdc-layout-grid">
+      <div class="mdc-layout-grid__inner">
+        <div class=" mdc-layout-grid__cell">
+          <a v-if="previousCompetence" class="footer-text" href="javascript:void(0)"
+             @click="selectCompetence(previousCompetence)">
+            <i class="material-icons arrow-L" aria-hidden="true">arrow_back</i>
+            <span class="direction">Previous</span><span class="screen-reader">:</span>
+            <div>{{previousCompetence.name}}</div>
+          </a>
+        </div>
+        <div class="mdc-layout-grid__cell"></div>
+        <div class="mdc-layout-grid__cell">
+          <a v-if="nextCompetence" class="footer-text" href="javascript:void(0)"
+             @click="selectCompetence(nextCompetence)">
+            <i class="material-icons arrow-R" aria-hidden="true">arrow_forward</i>
+            <span class="direction">Next</span><span class="screen-reader">:</span>
+            <div>{{nextCompetence.name}}</div>
+          </a>
+        </div>
       </div>
     </div>
+
   </footer>
 </template>
 
@@ -63,5 +59,13 @@
 </script>
 
 <style lang="scss">
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
 
+    a {
+      color: white;
+    }
+  }
 </style>
