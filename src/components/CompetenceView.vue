36fdac29002dea78c4f0a3f2a47a097be31986a4
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2017-12-12 15:45:43 +0200

    Seperate competence view to a new .vue component

---
+++ src/components/CompetenceView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="competence-breakdown">
+    <header class="mdc-toolbar mdc-elevation--z4">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <button class="demo-menu material-icons mdc-toolbar__menu-icon">menu</button>
+          <span class="mdc-toolbar__title catalog-title">competence name</span>
+        </section>
+      </div>
+    </header>
+    <main class="competence-main">
+      <h1 class="mdc-typography--display1">Title</h1>
+      <p class="mdc-typography--body1">content</p>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        competence: null
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss">
+
+</style>
--- src/skills.js
+++ src/skills.js
@@ -2,6 +2,7 @@ import '../scss/skills.scss'
 import Vue from 'vue'
 import * as mdc from  'material-components-web'
 import CompentencesMenu from './components/CompetencesMenu.vue'
+import CompentenceView from './components/CompetenceView.vue'
 
 //init material components web
 mdc.autoInit();
@@ -11,9 +12,12 @@ function initMenu() {
   let MDCPersistentDrawer = mdc.drawer.MDCPersistentDrawer;
   let drawer = new MDCPersistentDrawer(drawerEl);
   drawer.open = true;
-  document.querySelector('.demo-menu').addEventListener('click', function () {
+
+  //todo handle this via the event bus.
+
+/*  document.querySelector('.demo-menu').addEventListener('click', function () {
     drawer.open = !drawer.open;
-  });
+  });*/
 }
 
 initMenu();
@@ -22,4 +26,9 @@ initMenu();
 new Vue({
   el: '#competences-nav',
   render: h => h(CompentencesMenu)
+})
+
+new Vue({
+  el: '#competence-view',
+  render: h => h(CompentenceView)
 })
